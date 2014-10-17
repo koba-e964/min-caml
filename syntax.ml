e1bd9deed23beff4401e68c061ee0bd02eea1270
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2014-10-17 17:54:48 +0900

    Add debug print for Syntax.t

--- syntax.ml
+++ syntax.ml
@@ -25,3 +25,35 @@ type t = (* MinCamlの構文を表現するデータ型 (caml2html: syntax_t) *)
   | Get of t * t
   | Put of t * t * t
 and fundef = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
+
+let rec show_syntax_t (syn : t) : string =
+  match syn with
+    | Unit -> "()"
+    | Bool x -> string_of_bool x
+    | Int x  -> string_of_int x
+    | Float x -> string_of_float x
+    | Not x -> "not(" ^ show_syntax_t x ^ ")"
+    | Neg x -> "neg(" ^ show_syntax_t x ^ ")"
+    | Add (x, y) -> "+(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | Sub (x, y) -> "-(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | FNeg x -> "fneg(" ^ show_syntax_t x ^ ")"
+    | FAdd (x, y) -> "+.(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | FSub (x, y) -> "-.(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | FMul (x, y) -> "*.(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | FDiv (x, y) -> "/.(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | Eq (x, y) -> "=(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | LE (x, y) -> "<=(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | If (x, y, z) -> "if(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ "," ^ show_syntax_t z ^ ")"
+    | Let ((name, ty), y, z) -> "let(" ^ name ^ " := ...)"
+    | Var x -> "var(" ^ x ^ ")"
+    | LetRec (x,y) -> "letrec(" ^ show_fundef x ^ ", " ^ show_syntax_t y ^ ")"
+    | App (x, ls) -> "app(" ^ show_syntax_t x ^ ", " ^ List.fold_left (fun x y -> x ^ "," ^ show_syntax_t y) "" ls ^ ")"
+    | Tuple ls -> "tuple(" ^ List.fold_left (fun x y -> x ^ show_syntax_t y ^ ", ") "" ls ^ ")"
+    | LetTuple (ls, a, b) -> "let_tuple(...)"
+    | Array (x, y) -> "array(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | Get (x, y) -> "get(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ ")"
+    | Put (x, y, z) -> "put(" ^ show_syntax_t x ^ ", " ^ show_syntax_t y ^ "," ^ show_syntax_t z ^ ")"
+
+and show_fundef f = match f with
+  | { name = (id, ty); args = ls; body = body } -> "fundef(" ^ id ^ "," ^ Type.show_type_t ty ^ "" ^ "," ^ show_syntax_t body ^ ")"
+
--- type.ml
+++ type.ml
@@ -9,3 +9,14 @@ type t = (* MinCamlの型を表現するデータ型 (caml2html: type_t) *)
   | Var of t option ref
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
+
+let rec show_type_t (ty : t) = match ty with
+  | Unit -> "unit"
+  | Bool -> "bool"
+  | Int  -> "int"
+  | Float -> "float"
+  | Fun (ls, ty) -> "(" ^ List.fold_left (fun x y -> x ^ show_type_t y ^ ",") "" ls ^ "->" ^ show_type_t ty ^ ")"
+  | Tuple ls -> "(" ^ List.fold_left (fun x y -> x ^ show_type_t y ^ ",") "" ls ^ ")"
+  | Array ty -> "array(" ^ show_type_t ty ^ ")"
+  | Var t  -> "type_var"
+
