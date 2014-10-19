2efe6795345192989a5490ac6758f250e7f7b238
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2014-10-19 12:12:24 +0900

    Add .travis.yml to use travis-ci.com

---
+++ .travis-ci.sh
@@ -0,0 +1,19 @@
+case "$OCAML_VERSION" in
+4.00.1) ppa=avsm/ocaml40+opam10 ;;
+4.01.0) ppa=avsm/ocaml41+opam10 ;;
+*) echo "Unknown ocaml version:"$OCAML_VERSION; exit 1 ;;
+esac
+
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install ocaml
+arch/to_zebius
+make min-caml
+# Currently, no tests are available.
+# make test
+
+# Compiles test/fib.ml and displays the result.
+./min-caml test/fib
+echo "*** the content of assembly ***"
+cat test/fib.s
+
