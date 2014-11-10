d1b6fb4f5189f9ce3719265bdc0f732c01f61e36
koba-e964 <silentkiddie-2014@yahoo.co.jp>
2014-11-10 17:39:10 +0900

    Move fabs, fneg, fsqr, fhalf, abs_of_float to lib/fl.ml

--- lib/fl.ml
+++ lib/fl.ml
@@ -2,4 +2,9 @@ let rec fless x y = x < y +. 0.0
 let rec fispos x = x > 0.0
 let rec fisneg x = x < 0.0
 let rec fiszero x = x = 0.0
+let rec fneg x = -. x
+let rec fsqr x = x *. x
+let rec fhalf x = x *. 0.5
+let rec fabs x = if x < 0.0 then -. x else x
+let rec abs_float x = if x < 0.0 then -. x else x
 
